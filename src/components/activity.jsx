3b8530f7218bac704ba22fb01e7443423eb5ed1d
Aakash Tembhare <82498272+21-aakash@users.noreply.github.com>
2024-08-31 21:47:14 +0530

    Update activity.jsx

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -22,9 +22,9 @@ function Activity({ todos }) {
   const formattedData = getActivityData(todos);
 
   return (
-    <div className="mt-8 max-w-4xl mx-auto p-4 bg-gray-800  shadow-lg">
+    <div className="mt-8 max-w-4xl mx-auto p-4 bg-gray-900  shadow-lg">
       <h2 className="text-2xl font-semibold text-white mb-4">
-        Your Todo streak:{" "}
+        Todo streak:{" "}
       </h2>
 
       <CalendarHeatmap
@@ -52,3 +52,65 @@ function Activity({ todos }) {
 }
 
 export default Activity;
+
+
+
+
+
+
+
+
+// import React from "react";
+// import CalendarHeatmap from "react-calendar-heatmap";
+// import "react-calendar-heatmap/dist/styles.css"; // Import the default styles
+
+// function Activity({ todos }) {
+//   console.log(todos);
+//   // Process to-do data to get activity counts by date
+//   const getActivityData = (todos) => {
+//     const activityCounts = {};
+
+//     todos.forEach((todo) => {
+//       const date = new Date(todo.createdAt).toISOString().split("T")[0]; // Format date as YYYY-MM-DD
+//       activityCounts[date] = (activityCounts[date] || 0) + 1;
+//     });
+
+//     return Object.keys(activityCounts).map((date) => ({
+//       date: new Date(date),
+//       count: activityCounts[date],
+//     }));
+//   };
+
+//   const formattedData = getActivityData(todos);
+
+//   return (
+//     <div className="mt-8 max-w-4xl mx-auto p-4 bg-gray-800  shadow-lg">
+//       <h2 className="text-2xl font-semibold text-white mb-4">
+//         Your Todo streak:{" "}
+//       </h2>
+
+//       <CalendarHeatmap
+//         startDate={new Date("2024-01-01")}
+//         endDate={new Date()}
+//         values={formattedData}
+//         classForValue={(value) => {
+//             if (!value) {
+//               return "color-empty"; // No data
+//             }
+//             const count = value.count;
+//             if (count > 5) {
+//               return "color-scale-high"; // High activity
+//             } else if (count >= 3) {
+//               return "color-scale-mid"; // Medium activity
+//             } else if (count <= 2) {
+//               return "color-scale-low"; // Low activity
+//             }
+//             return "color-empty"; // Default case (though this may not be needed)
+//           }}
+          
+//       />
+//     </div>
+//   );
+// }
+
+// export default Activity;
